ae32dbeb418bf44c6866c0a4e41cd6d4285811a0
i1ya97 <31565819+i1ya97@users.noreply.github.com>
2020-01-15 13:05:19 +0700

    added expanded list

--- src/app/cluster-list/cluster-list.component.ts
+++ src/app/cluster-list/cluster-list.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+
 
 @Component({
   selector: 'app-cluster-list',
@@ -12,16 +13,21 @@ export class ClusterListComponent {
   colorGray = '#aaaaaa';
   positionY = 0;
   items  = [
-    {startX: 500, startWidth: 50, criticalX: 550, criticalWidth: 20, finalX: 570, finalWidth: 350, name: 'CY_k116'
-      , interval: 'N/A', type: 'N/A', RRSU: 212, oil: 280, rate: 1500, WC : 81.1},
-    {startX: 600, startWidth: 50, criticalX: 650, criticalWidth: 40, finalX: 690, finalWidth: 350, name: 'CY_k111'
-      , interval: 'N/A', type: 'N/A', RRSU: 212, oil: 217.5, rate: 1675, WC : 87},
-    {startX: 750, startWidth: 50, criticalX: 800, criticalWidth: 20, finalX: 820, finalWidth: 350, name: 'CY_k117'
-      , interval: 'N/A', type: 'N/A', RRSU: 212, oil: 150, rate: 800, WC : 81.2},
-    {startX: 850, startWidth: 100, criticalX: 950, criticalWidth: 20, finalX: 970, finalWidth: 290, name: 'CY_k118'
-      , interval: 'N/A', type: 'N/A', RRSU: 212, oil: 300, rate: 600, WC : 50}
+    {id: 0, startX: 500, startWidth: 50, criticalX: 550, criticalWidth: 20, finalX: 570, finalWidth: 350, name: 'CY_k116'
+      , interval: 'N/A', type: 'N/A', RRSU: 212, oil: 280, rate: 1500, WC : 81.1, hide: false },
+    {id: 1, startX: 600, startWidth: 50, criticalX: 650, criticalWidth: 40, finalX: 690, finalWidth: 350, name: 'CY_k111'
+      , interval: 'N/A', type: 'N/A', RRSU: 212, oil: 217.5, rate: 1675, WC : 87, hide: false },
+    {id: 2, startX: 750, startWidth: 50, criticalX: 800, criticalWidth: 20, finalX: 820, finalWidth: 350, name: 'CY_k117'
+      , interval: 'N/A', type: 'N/A', RRSU: 212, oil: 150, rate: 800, WC : 81.2, hide: false },
+    {id: 3, startX: 850, startWidth: 100, criticalX: 950, criticalWidth: 20, finalX: 970, finalWidth: 290, name: 'CY_k118'
+      , interval: 'N/A', type: 'N/A', RRSU: 212, oil: 300, rate: 600, WC : 50, hide: true }
   ];
 
   constructor() { }
 
+
+  onChange(id: number) {
+    const idx = this.items.findIndex(t => t.id === id);
+    this.items[idx].hide = !this.items[idx].hide;
+  }
 }
