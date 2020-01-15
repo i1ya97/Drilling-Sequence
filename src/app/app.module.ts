5f4b5027e2cc527a2925facb171b6fb763bbbb04
i1ya97 <31565819+i1ya97@users.noreply.github.com>
2020-01-15 21:51:51 +0700

    added the entity 'work'

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,13 +3,13 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { ClusterListComponent } from './cluster-list/cluster-list.component';
-import { WellListComponent } from './well-list/well-list.component';
+import { PadListComponent } from './pad-list/pad-list.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     ClusterListComponent,
-    WellListComponent
+    PadListComponent
   ],
   imports: [
     BrowserModule
